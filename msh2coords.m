a1fadff2c68d645fb2279636f81cff98d792c4ed
Jack Loveless <jloveles@smith.edu>
2020-07-27 10:08:09 -0400

    Update msh2coords.m

--- msh2coords.m
+++ msh2coords.m
@@ -96,4 +96,63 @@ end
 % Trim empty rows
 v = elkeep(sum(elkeep, 2) ~= 0, :);
 
+%%%%%%%%%%%%%%%
+% Version 4.1 %
+%%%%%%%%%%%%%%%
+elseif strcmp(in(2, 1:2), '4.')
+
+% Find flags
+flags = findstr(in(:, 1)', '$');
+
+% Nodes start at 5th flag + 1, end at 6th - 1
+nodekeep = str2num(in(flags(5)+1, :)); % Get number of nodes
+nodekeep = zeros(nodekeep(2), 3); % Allocate space for node array
+nodes = in(flags(5)+2:flags(6)-1, :);
+
+j = 1;
+% Step through node rows
+while j < size(nodes, 1)
+  % Get length of entity
+  lent = str2num(nodes(j, :)); lent = lent(end);
+  % Loops through that entity's nodes
+  for i = (j+lent) + (1:lent)
+     % Convert to numbers
+     nodetest = str2num(nodes(i, :));
+     % Check size
+     if size(nodetest, 2) == 3 % If it contains 3 coordinates
+        idx = str2num(nodes(i-lent, :));
+        nodekeep(idx, :) = nodetest; % Save its coordinates
+     end
+     j = i+1;
+  end
+end
+c = nodekeep;
+
+% Elements start at 7th flag + 1, end at 8th flag - 1
+elkeep = str2num(in(flags(7)+1, :)); % Get number of elements
+elkeep = zeros(elkeep(2), 3); % Allocate space for element array
+els = in(flags(7)+2:flags(8)-1, :); % Get all element rows
+
+j = 1;
+% Step through element rows
+while j < size(els, 1)
+  % Get length of entity
+  lent = str2num(els(j, :)); lent = lent(end);
+  % Loops through that entity's elements
+  for i = j+(1:lent)
+     % Convert to numbers
+     eltest = str2num(els(i, :));
+     % Check size
+     if size(eltest, 2) == 4 % If it's a triangle,
+        elkeep(eltest(1), :) = eltest(2:4); % Save its nodes
+     end
+     j = i+1;
+  end
+end
+% Trim empty rows
+v = elkeep(sum(elkeep, 2) ~= 0, :);
+
+
+
+
 end
